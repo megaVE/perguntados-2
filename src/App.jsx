4955878c2ac5c2ba19a839abc700c4adb04cf142
megaVE <vinicius.honorio@sou.unifal-mg.edu.br>
2023-11-30 17:28:17 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Question from './components/Question'
+
+function App() {
+    return (<>
+        <h1>Não é o Perguntados</h1>
+        <Question category=""/>
+    </>)
+}
+
+export default App
